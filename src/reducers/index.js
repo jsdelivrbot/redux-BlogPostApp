db314eefef67172f61be1bc1e846920c27671fdf
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-23 16:28:46 +1000

    wired up redux-forms to the index.js reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
 
+// we are assigning reducer to the alias formReducer
+// this is to prevent errors if we are to import something called reducer
+import { reducer as formReducer } from 'redux-form';
+
 import PostsReducer from './reducer_posts';
 
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
