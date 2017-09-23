e5475d964facb57aecf219cf542538a6476986b4
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-23 16:09:48 +1000

    Added link button/ link react-router-dom tag to allow for movement between components

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
-//Actions
+
+//ACTIONS
 import { fetchPosts } from './../actions'; 
 
 
 
 class PostIndex extends React.Component{
     componentDidMount(){
+
         // this function will run/called when the component is viewable on the DOM
         // perfect place to load data, or initiate a one time loading procedure 
 
@@ -38,6 +41,13 @@ class PostIndex extends React.Component{
 
         return(
             <div>
+                <div className="text-xs-right">
+                    {/*className will pull the div to the right of the screen */}
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
