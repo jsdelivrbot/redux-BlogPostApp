48869c3c8038bcb41452657c45f357e0a00ba648
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-23 14:09:40 +1000

    connected component to reducer & used a new method to mapDispatchToProps

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+//Actions
+import { fetchPosts } from './../actions'; 
+
 
 
 class PostIndex extends React.Component{
+    componentDidMount(){
+        // this function will run/called when the component is viewable on the DOM
+        // perfect place to load data, or initiate a one time loading procedure 
+
+        // we are using this vs componentWillMount() as we would prefer the component to be loaded, 
+        // before we make the API request this is due to the fact that API requests 
+        // can take a few secs & we do not want the component loading
+
+        this.props.fetchPosts();
+
+
+    }
     render(){
         return(
             <div>
@@ -12,4 +29,6 @@ class PostIndex extends React.Component{
     }
 }
 
-export default PostIndex;
+
+//Passing action through Arguments
+export default connect(null, {fetchPosts : fetchPosts})(PostIndex);
