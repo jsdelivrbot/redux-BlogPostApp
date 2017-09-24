e029d20fa7d9f3b45a0b5f5101af71fec87be7e6
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-24 18:28:23 +1000

    implement redux-form to PostNew component

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -2,16 +2,52 @@
 
 import React, { Component } from 'react';
 
+// reduxForm uses the same logic as connect from - react-redux
+// Field - this is the component we need to make for each piece of state 
+import { Field, reduxForm } from 'redux-form';
+
 
 
 class PostsNew extends Component {
-    render() {
-        return (
+    renderTitleField(field){
+        //argument being called must be field
+        //field argument has event handlers
+        //that allow us to render through field
+
+        return(
             <div>
-                PostsNew!
+                <input
+                    type="text"
+                    // object contains, onChange, onFocus, onBlur={field.input.onBlur}
+                    {...field.input}
+                    
+                />
             </div>
+        );
+
+    }
+
+    render() {
+        // name = will be the name of the state of the field
+        // component = takes in a function or another component 
+        // that will be used to display the field component
+        return (
+            <form>
+                <Field 
+                name="title" 
+                component={this.renderTitleField}
+                />
+            </form>
         )
     }
 }
 
-export default PostsNew;
+
+// reduxForm - requires a single argument function(){  }
+// reduxForm - takes configuration options
+// form option will be the form for this form
+// make sure that the string is Uniqe - each form must be called something new
+// this is because if the form is the same, then both forms will merge ---
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 // we are assigning reducer to the alias formReducer
 // this is to prevent errors if we are to import something called reducer
-import { reducer as formReducer } from 'redux-form';
+import {reducer as formReducer} from 'redux-form'
 
 import PostsReducer from './reducer_posts';
 
